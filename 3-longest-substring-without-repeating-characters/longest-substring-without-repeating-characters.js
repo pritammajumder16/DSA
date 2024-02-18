ed4db0acf290fa585427a58992fd5e3110c48ef6
pritam16 <pritammajumder416@gmail.com>
2024-02-18 20:00:14 +0530

    Time: 98 ms (37.26%) | Memory: 54.7 MB (27.64%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -8,23 +8,21 @@ var lengthOfLongestSubstring = function (s) {
     let startIndex = 0;
     let longestLength = 0;
     let currLength = 0;
-    while (i < s.length) { 
-        if (!hashMap.hasOwnProperty(s[i])) {
-            hashMap[s[i]] = i;
+    while (i < s.length) {
+        if (!hashMap.hasOwnProperty(s[i])|| hashMap[s[i]]<startIndex) { //nomatch
             currLength++;
-            i++;
-        } else {
+        } else {  //restart 
             if (currLength > longestLength) {
                 longestLength = currLength;
             }
-            currLength = 0;
             startIndex = hashMap[s[i]] + 1;
-            hashMap = {}
-            i = startIndex;
-            if (s.length - i < longestLength) {
+            currLength = i-startIndex+1;
+            if (s.length - startIndex < longestLength) {
                 return longestLength;
             }
         }
+        hashMap[s[i]] = i;
+        i++;
     }
     if (currLength > longestLength) {
         return currLength;
