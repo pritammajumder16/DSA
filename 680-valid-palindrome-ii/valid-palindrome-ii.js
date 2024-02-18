dd08607fb12581189a3f56c448456dad89eb4dce
pritam16 <pritammajumder416@gmail.com>
2024-02-19 02:00:29 +0530

    Time: 68 ms (73.59%) | Memory: 54.6 MB (50.21%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -3,11 +3,11 @@
  * @return {boolean}
  */
 var validPalindrome = function(s) {
-    let obj= checkPalindromeAndReturnIndex(s)
+    let obj= checkPalindromeAndReturnIndex(s,0,s.length-1)
     if(obj.check==false){
-        if(checkPalindromeAndReturnIndex(s.substring(0,obj.startIndex)+s.substring(obj.startIndex+1)).check){
+        if(checkPalindromeAndReturnIndex(s,obj.startIndex+1,obj.endIndex).check){
             return true;
-        }else if(checkPalindromeAndReturnIndex(s.substring(0,obj.endIndex)+s.substring(obj.endIndex+1)).check){
+        }else if(checkPalindromeAndReturnIndex(s,obj.startIndex,obj.endIndex-1).check){
             return true;
         }
         else return false;
@@ -15,8 +15,9 @@ var validPalindrome = function(s) {
     return true;
 };
 
-const checkPalindromeAndReturnIndex = function(s){
- for (let i = 0, j=s.length-1; i <Math.floor(s.length/2);i++,j--){
+const checkPalindromeAndReturnIndex = function(s,start,end){
+ let length = Math.floor((start+end+1)/2);
+ for (let i = start, j=end; i <length;i++,j--){
         if(s[i]!=s[j]){
             return {check: false, startIndex: i, endIndex: j}
         }
