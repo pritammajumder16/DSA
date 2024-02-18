434d49d4157f1b7ef40e066b3b6636ef45a2eb40
pritam16 <pritammajumder416@gmail.com>
2024-02-18 19:19:28 +0530

    Time: 309 ms (9.77%) | Memory: 67.4 MB (5.00%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let hashMap = {};
+    let i = 0;
+    let startIndex = 0;
+    let longestLength = 0;
+    let currLength = 0;
+    while (i < s.length) { //1   {d:0}
+        if (!hashMap.hasOwnProperty(s[i])) {
+            hashMap[s[i]] = i;
+            currLength++;
+            i++;
+        } else {
+            if (currLength > longestLength) {
+                longestLength = currLength;
+            }
+            currLength = 0;
+            startIndex = hashMap[s[i]] + 1;
+            
+            hashMap = {}
+            i = startIndex;
+            if (s.length - i < longestLength) {
+                return longestLength;
+            }
+        }
+    }
+    if (currLength > longestLength) {
+        longestLength = currLength;
+    }
+    return longestLength;
+};
