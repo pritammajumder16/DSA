affaa7284891c17557852cbdb3c10c56c024dcdf
pritam16 <pritammajumder416@gmail.com>
2024-02-23 04:17:07 +0530

    Time: 59 ms (55.49%) | Memory: 51.4 MB (29.13%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -11,53 +11,53 @@
  * @return {number[]}
  */
 //           BFS SOLUTION
-// var rightSideView = function(root) {
-//     if(!root) return []
-//     const bfsQueue = [root];
-//     const rightMostValues = []
-//     while(bfsQueue.length){
-//         let count = 0;
-//         const levelNumberOfNodes = bfsQueue.length;
-//         let rightMostNode= null;
-//         while(count<levelNumberOfNodes){
-//             const currentNode = bfsQueue.shift()
-//             if(!rightMostNode)
-//             rightMostNode = currentNode;
-//             if(currentNode.right){
-//                 bfsQueue.push(currentNode.right)
-//             }
-//             if(currentNode.left){
-//                 bfsQueue.push(currentNode.left)
-//             }
-//             count++;
-//         }
-//         if(rightMostNode) rightMostValues.push(rightMostNode.val)
-//     }
-//     return rightMostValues;
-// }
+var rightSideView = function(root) {
+    if(!root) return []
+    const bfsQueue = [root];
+    const rightMostValues = []
+    while(bfsQueue.length){
+        let count = 0;
+        const levelNumberOfNodes = bfsQueue.length;
+        let rightMostNode= null;
+        while(count<levelNumberOfNodes){
+            const currentNode = bfsQueue.shift()
+            if(!rightMostNode)
+            rightMostNode = currentNode;
+            if(currentNode.right){
+                bfsQueue.push(currentNode.right)
+            }
+            if(currentNode.left){
+                bfsQueue.push(currentNode.left)
+            }
+            count++;
+        }
+        if(rightMostNode) rightMostValues.push(rightMostNode.val)
+    }
+    return rightMostValues;
+}
 
 
 //              DFS Solution
-var rightSideView = function (root) {
-    if (!root) return [];
-    const result = []
-     dfsRightSideView(root, 0, result);
-     return result;
-}
-const dfsRightSideView = (root, currentLevel, result) => {
-    if (!root) return;
-    currentLevel++;
-    if (result[currentLevel-1]==undefined) {
-        result[currentLevel-1]=root.val;
-    }
-    console.log(root, currentLevel, result, root.right)
-    if (root.right) {
-        dfsRightSideView(root.right, currentLevel, result)
-    }
-    if (root.left) {
-        dfsRightSideView(root.left, currentLevel, result)
-    }
+// var rightSideView = function (root) {
+//     if (!root) return [];
+//     const result = []
+//      dfsRightSideView(root, 0, result);
+//      return result;
+// }
+// const dfsRightSideView = (root, currentLevel, result) => {
+//     if (!root) return;
+//     currentLevel++;
+//     if (result[currentLevel-1]==undefined) {
+//         result[currentLevel-1]=root.val;
+//     }
+//     console.log(root, currentLevel, result, root.right)
+//     if (root.right) {
+//         dfsRightSideView(root.right, currentLevel, result)
+//     }
+//     if (root.left) {
+//         dfsRightSideView(root.left, currentLevel, result)
+//     }
 
-    return 1;
+//     return 1;
 
-}
+// }
