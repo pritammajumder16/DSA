2c32794d455a58215170c364aa1a18bea9004cec
pritam16 <pritammajumder416@gmail.com>
2024-02-22 06:14:32 +0530

    Time: 58 ms (98.54%) | Memory: 57.2 MB (39.48%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -4,13 +4,14 @@
  */
 var minRemoveToMakeValid = function (s) {
     const stack = [];
+    s= s.split("")
     for (let i = 0; i < s.length; i++) {
         if (s[i] == "(") {
             stack.push(i)
         } else if (s[i] == ")") {
             let charAtPop = stack.pop()
             if (!charAtPop && charAtPop != 0) {
-                s = s.substring(0, i) + s.substring(i + 1, s.length);
+                s[i] = "";
                 i--;
             }
         }
@@ -18,8 +19,9 @@ var minRemoveToMakeValid = function (s) {
     }
     if (!stack.length == 0) {
         for (let j = stack.length - 1; j >= 0; j--) {
-            s = s.substring(0, stack[j]) + s.substring(stack[j] + 1, s.length)
+            s[stack[j]] = ""
         }
     }
+    s=s.join("")
     return s;
 };
