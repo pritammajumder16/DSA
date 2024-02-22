ff8e03b68a8f97b19f9867df8fa58707c644f5fc
pritam16 <pritammajumder416@gmail.com>
2024-02-23 03:23:05 +0530

    Time: 43 ms (98.65%) | Memory: 51.2 MB (40.11%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ //BFS SOLUTION
+var rightSideView = function(root) {
+    if(!root) return []
+    let rightSideViewArr = calcRightSideView(root)
+    return rightSideViewArr;
+};
+const calcRightSideView = (node)=>{
+    const bfsQueue = [node];
+    const rightMostValues = []
+    while(bfsQueue.length){
+        let count = 0;
+        const levelNumberOfNodes = bfsQueue.length;
+        let rightMostNode= null;
+        while(count<levelNumberOfNodes){
+            const currentNode = bfsQueue.shift()
+            if(!rightMostNode)
+            rightMostNode = currentNode;
+            if(currentNode.right){
+                bfsQueue.push(currentNode.right)
+            }
+            if(currentNode.left){
+                bfsQueue.push(currentNode.left)
+            }
+            count++;
+        }
+        if(rightMostNode) rightMostValues.push(rightMostNode.val)
+    }
+    return rightMostValues;
+}
