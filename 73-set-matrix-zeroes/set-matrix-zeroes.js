9bb4a44a3f030aa76760b03fd9464fbbf0665621
pritam16 <pritammajumder416@gmail.com>
2024-03-01 06:25:26 +0530

    Time: 86 ms (18.16%) | Memory: 57.7 MB (5.08%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -4,27 +4,42 @@
   /3 = 0  3% 3%
 
  */
+ const DIRECTIONS = [[-1,0],[0,1],[1,0],[0,-1]]
 var setZeroes = function (matrix) {
-    let lengthOfColumns = matrix[0].length;
-    let hashMap = {}
-    for (let i = 0; i < matrix.length * lengthOfColumns; i++) {
-        let row = Math.floor(i / lengthOfColumns);
-        let column = i - row * lengthOfColumns;
-        if (matrix[row][column] == 0) {
-            hashMap[row + "r"] = true;
-            hashMap[column + "c"] = true;
-        }
-
-    }
-    for (let i = 0; i < matrix.length * lengthOfColumns; i++) {
-        let row = Math.floor(i / lengthOfColumns);
-        let column = i - row * lengthOfColumns;
-        if (hashMap[row + "r"]) {
-            matrix[row][column] = 0;
-        }
-        if (hashMap[column + "c"]) {
-            matrix[row][column] = 0;
+    let seen = new Array(matrix.length).fill(0).map(()=>new Array(matrix[0].length).fill(false))
+    let toSearch = []
+    for(let i = 0; i <matrix.length;i++){
+        for(let j = 0; j < matrix[0].length;j++){
+            if(matrix[i][j]==0)
+                toSearch.push([i,j])
         }
     }
+    toSearch.forEach(search=>{
+        seen[search[0]][search[1]]=false;
+        matrix = dfsChangeMat(matrix,search[0],search[1],null,seen)
+    })
     return matrix;
 };
+
+const dfsChangeMat = function(matrix,row,col,dir,seen){
+
+    seen[row][col]=true;
+    if(dir==null)
+    {
+        DIRECTIONS.forEach((dir,index)=>{
+            const newRow= row+dir[0]
+            const newCol = col+dir[1]
+            if(newRow>=0 && newRow<matrix.length&&newCol>=0&&newCol<matrix[0].length){
+                return dfsChangeMat(matrix,newRow,newCol,index,seen)
+            }
+        })
+    }else{    
+         matrix[row][col]=0;
+         const newRow= row+DIRECTIONS[dir][0]
+            const newCol = col+DIRECTIONS[dir][1]
+            if(newRow>=0 && newRow<matrix.length&&newCol>=0&&newCol<matrix[0].length){
+                return dfsChangeMat(matrix,newRow,newCol,dir,seen)
+            }
+    }
+    return matrix
+}
