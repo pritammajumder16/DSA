17965f97c2effcc95361dd5d2351c15a3091b54a
pritam16 <pritammajumder416@gmail.com>
2024-03-03 21:22:30 +0530

    Time: 92 ms (20.11%) | Memory: 54.9 MB (50.36%) - LeetSync

---
+++ 207-course-schedule/course-schedule.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    let adjList = new Array(numCourses)
+    let inDegree = new Array(numCourses).fill(0)
+    prerequisites.forEach(ele=>{
+        if(!adjList[ele[1]])adjList[ele[1]]=[]
+        adjList[ele[1]].push(ele[0])
+        inDegree[ele[0]]++;
+    })  
+    console.log(adjList,inDegree)
+    let stack = []
+    inDegree.forEach((ele,index)=>{
+        if(ele == 0)
+            stack.push(index)
+    })
+    while(stack.length){
+        let currentElement= stack.pop();
+        adjList[currentElement]?.forEach(elem=>{
+            inDegree[elem]--;
+            if(inDegree[elem]==0){
+                stack.push(elem)
+            }
+        })
+    }
+    console.log(inDegree)
+    for(let i = 0; i<inDegree.length;i++){
+        if(inDegree[i]>0){
+            return false
+        }
+    }
+    return true
+};
