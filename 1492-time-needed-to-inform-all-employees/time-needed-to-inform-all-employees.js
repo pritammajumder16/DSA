e2f6c5cd6875b23e528acb64767c0ff896fc9564
pritam16 <pritammajumder416@gmail.com>
2024-03-03 20:47:33 +0530

    Time: 1476 ms (5.05%) | Memory: 88.7 MB (31.19%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @param {number} headID
+ * @param {number[]} manager
+ * @param {number[]} informTime
+ * @return {number}
+ */
+var numOfMinutes = function(n, headID, manager, informTime) {
+    let adjList = manager.map(()=>[])
+    let seen = manager.map(()=>false)
+    for(let i = 0;i<n;i++){
+        if(manager[i]!=-1){
+        adjList[manager[i]].push(i)}
+    }
+    console.log(adjList)
+    return traverseDFS(headID,adjList,informTime,seen);
+};
+
+const traverseDFS = (currId,adjList,informTime,seen)=>{
+    if(seen[currId])return;
+    seen[currId]=true;
+    let maxCost = 0;
+    for(let i = 0; i < adjList[currId].length;i++){
+        maxCost = Math.max(maxCost,traverseDFS(adjList[currId][i],adjList,informTime,seen))
+    }
+    console.log("return", maxCost , currId)
+    return maxCost+informTime[currId];
+}
