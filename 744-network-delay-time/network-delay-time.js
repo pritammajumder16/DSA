e40a044b25229df70e04ccbe294da3cfe8988e1a
pritam16 <pritammajumder416@gmail.com>
2024-03-04 05:20:02 +0530

    Time: 86 ms (88.55%) | Memory: 56.5 MB (74.85%) - LeetSync

--- 744-network-delay-time/network-delay-time.js
+++ 744-network-delay-time/network-delay-time.js
@@ -5,80 +5,7 @@
  * @return {number}
  */
 //Dijasktra's algorithm
-class PQ {
-    constructor(comparator = (a, b) => a > b) {
-        this._heap = [];
-        this._comparator = comparator;
-    }
 
-  _compare(i, j) {
-    return this._comparator(this._heap[i], this._heap[j]);
-  }
-  _swap(i, j) {
-    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
-  }
-
-    size() {
-        return this._heap.length;
-    }
-    push(value) {
-        this._heap.push(value);
-        this._siftUp();
-
-        return this.size();
-    }
-
-    _siftUp() {
-        let nodeIdx = this.size() - 1;
-
-        while (0 < nodeIdx && this._compare(nodeIdx, this._parent(nodeIdx))) {
-            this._swap(nodeIdx, this._parent(nodeIdx));
-            nodeIdx = this._parent(nodeIdx);
-        }
-    }
-
-    _parent(idx) {
-        return Math.floor((idx - 1) / 2);
-    }
-
-    _leftChild(idx) {
-        return idx * 2 + 1;
-    }
-
-    _rightChild(idx) {
-        return idx * 2 + 2;
-    }
-
-    _siftDown() {
-        let nodeIdx = 0;
-
-        while (
-            (this._leftChild(nodeIdx) < this.size() &&
-                this._compare(this._leftChild(nodeIdx), nodeIdx)) ||
-            (this._rightChild(nodeIdx) < this.size() &&
-                this._compare(this._rightChild(nodeIdx), nodeIdx))
-        ) {
-            const greaterChildIdx =
-                this._rightChild(nodeIdx) < this.size() &&
-                    this._compare(this._rightChild(nodeIdx), this._leftChild(nodeIdx))
-                    ? this._rightChild(nodeIdx)
-                    : this._leftChild(nodeIdx);
-
-            this._swap(greaterChildIdx, nodeIdx);
-            nodeIdx = greaterChildIdx;
-        }
-
-    }
-    pop() {
-        if (this.size() > 1) {
-            this._swap(0, this.size() - 1);
-        }
-
-        const poppedValue = this._heap.pop();
-        this._siftDown();
-        return poppedValue;
-    }
-}
 
 var networkDelayTime = function (times, n, k) {
     let adjList = new Array(n)
@@ -86,11 +13,11 @@ var networkDelayTime = function (times, n, k) {
         if (!adjList[times[i][0]-1]) adjList[times[i][0]-1] = []
         adjList[times[i][0]-1].push([times[i][1]-1, times[i][2]])
     }
-    const heap = new PQ((a, b) => distances[a] < distances[b])
+    const heap = []
     heap.push(k-1)
     let distances = new Array(n).fill(Infinity)
     distances[k-1] = 0;
-    while (heap.size()) {
+    while (heap.length) {
         const currentElement = heap.pop();
         adjList[currentElement]?.forEach((element) => {
             //element[0] - Neighbouring vertex element[1]weight to it
