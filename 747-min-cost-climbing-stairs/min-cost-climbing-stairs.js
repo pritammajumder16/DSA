e7c2e2cbad812e6c0835b9b13e604b4862772b2b
pritam16 <pritammajumder416@gmail.com>
2024-03-04 22:51:17 +0530

    Time: 61 ms (34.95%) | Memory: 49.2 MB (67.21%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -8,11 +8,10 @@ var minCostClimbingStairs = function(cost) {
     let minCost=0;
     if(cost.length==1)return dpOne;
     if(cost.length==2) return dpOne>dpTwo?dpTwo:dpOne;
-    cost.push(0)
     for(let i = 2 ; i < cost.length; i ++){
         minCost=cost[i]+Math.min(dpOne,dpTwo);
         dpOne=dpTwo;
         dpTwo = minCost;
     }
-    return minCost
+    return Math.min(dpOne,dpTwo)
 };
