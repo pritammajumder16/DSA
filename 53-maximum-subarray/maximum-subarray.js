fa8088c04f034013e72859d7ca3b020d9cab7cbf
pritam16 <pritammajumder416@gmail.com>
2024-03-05 04:51:39 +0530

    Time: 77 ms (31.82%) | Memory: 57.7 MB (41.21%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,16 +2,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-      if (nums.length === 0) return 0;
-    
-    let maxSum = nums[0];
-    let currentSum = nums[0];
-    
-    for (let i = 1; i < nums.length; i++) {
-        currentSum = Math.max(nums[i], currentSum + nums[i]);
-        maxSum = Math.max(maxSum, currentSum);
+var maxSubArray = function (nums) {
+    let currentSum = nums[0]
+    let largestSum = nums[0]
+    for(let i = 1; i < nums.length;i++){
+        currentSum = Math.max(nums[i],currentSum+nums[i])
+        largestSum = Math.max(largestSum,currentSum)
     }
-    
-    return maxSum;
+    return largestSum;
 };
