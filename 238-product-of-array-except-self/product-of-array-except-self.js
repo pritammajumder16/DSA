42c294f71754e24b19b97027cbb7207f14e6f2c0
pritam16 <pritammajumder416@gmail.com>
2024-09-21 15:30:31 +0530

    Time: 505 ms (5.05%) | Memory: 90.6 MB (5.02%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    const productsFromLeft = {};
+    const productsFromRight = {};
+    const products = []
+    for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
+        if (i == 0)
+            productsFromLeft[i] = 1
+        else if (i == 1) {
+            productsFromLeft[i] = nums[i - 1]
+        }
+        else {
+            productsFromLeft[i] = productsFromLeft[i - 1] * nums[i - 1]
+        }
+        if (j == nums.length - 1)
+            productsFromRight[j] = 1
+        else if (j == nums.length - 2) {
+            productsFromRight[j] = nums[j + 1]
+        }
+        else {
+            productsFromRight[j] = productsFromRight[j + 1] * nums[j + 1]
+        }
+    }
+    console.log(productsFromLeft, productsFromRight)
+    for (let i = 0; i < nums.length; i++) {
+        if (i == 0)
+            products[i] = productsFromRight[i]
+        else if (i == nums.length - 1) {
+            products[i] = productsFromLeft[i]
+        } else {
+            products[i] = productsFromRight[i] * productsFromLeft[i]
+        }
+    }
+    return products;
+};
