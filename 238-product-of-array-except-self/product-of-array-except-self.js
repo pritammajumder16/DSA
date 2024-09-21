a94dff3610328f15f81ea8b15def7145595368c3
pritam16 <pritammajumder416@gmail.com>
2024-09-21 15:52:23 +0530

    Time: 104 ms (56.70%) | Memory: 63.2 MB (72.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,30 +3,14 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    const productsFromLeft = {};
-    const productsFromRight = {};
-    const products = []
+    let prefix = 1;
+    let suffix = 1;
+    const products = new Array(nums.length).fill(1)
     for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
-        if (i == 0)
-            productsFromLeft[i] = 1
-        else {
-            productsFromLeft[i] = productsFromLeft[i - 1] * nums[i - 1]
-        }
-        if (j == nums.length - 1)
-            productsFromRight[j] = 1
-        else {
-            productsFromRight[j] = productsFromRight[j + 1] * nums[j + 1]
-        }
-    }
-    // console.log(productsFromLeft, productsFromRight)
-    for (let i = 0; i < nums.length; i++) {
-        if (i == 0)
-            products[i] = productsFromRight[i]
-        else if (i == nums.length - 1) {
-            products[i] = productsFromLeft[i]
-        } else {
-            products[i] = productsFromRight[i] * productsFromLeft[i]
-        }
+        products[i] = products[i] * prefix;
+        products[j] = products[j] * suffix;
+        prefix = prefix * nums[i];
+        suffix = suffix * nums[j]
     }
     return products;
 };
