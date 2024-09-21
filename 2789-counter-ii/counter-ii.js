d54bbdd53a0f5bda89d8b81d289a22eb24bb0611
pritam16 <pritammajumder416@gmail.com>
2024-09-22 02:05:37 +0530

    Time: 61 ms (43.77%) | Memory: 51.9 MB (38.59%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const initialValue = init;
+    return {
+        increment:()=>{
+           return  ++init;
+        },
+        decrement:()=>{
+            return --init;
+        },
+        reset:()=>{
+            init = initialValue
+            return initialValue
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
