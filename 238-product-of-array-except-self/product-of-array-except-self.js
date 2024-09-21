0808a6195ac0ddf58557a3da88e0fe16578f9bc7
pritam16 <pritammajumder416@gmail.com>
2024-09-21 15:37:04 +0530

    Time: 151 ms (6.31%) | Memory: 67.9 MB (10.36%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -9,18 +9,16 @@ var productExceptSelf = function (nums) {
     for (let i = 0, j = nums.length - 1; i < nums.length; i++, j--) {
         if (i == 0)
             productsFromLeft[i] = 1
-    
         else {
             productsFromLeft[i] = productsFromLeft[i - 1] * nums[i - 1]
         }
         if (j == nums.length - 1)
             productsFromRight[j] = 1
-
         else {
             productsFromRight[j] = productsFromRight[j + 1] * nums[j + 1]
         }
     }
-    console.log(productsFromLeft, productsFromRight)
+    // console.log(productsFromLeft, productsFromRight)
     for (let i = 0; i < nums.length; i++) {
         if (i == 0)
             products[i] = productsFromRight[i]
