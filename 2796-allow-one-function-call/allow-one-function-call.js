898a8872b74d7ff2147826d323f94414010fc1cb
pritam16 <pritammajumder416@gmail.com>
2024-09-22 04:31:18 +0530

    Time: 50 ms (68.87%) | Memory: 49.1 MB (21.33%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    return function(...args){
+        if(called==true){
+            return undefined
+        }
+        called=true;
+        return fn(...args)
+
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
