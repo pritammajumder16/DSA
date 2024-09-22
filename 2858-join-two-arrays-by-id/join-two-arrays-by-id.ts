0c787f77898b2e171cadb4a5a0933851e78b93f6
pritam16 <pritammajumder416@gmail.com>
2024-09-22 22:39:48 +0530

    Time: 346 ms (51.30%) | Memory: 97.8 MB (27.66%) - LeetSync

--- 2858-join-two-arrays-by-id/join-two-arrays-by-id.ts
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.ts
@@ -4,16 +4,13 @@ type ArrayType = { "id": number } & Record<string, JSONValue>;
 function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
     const map = new Map<number, ArrayType>();
     arr2.forEach(item => map.set(item.id, item));
-    let result: ArrayType[] = []
     arr1.forEach(item => {
         if (map.has(item.id)) {
-            result.push({ ...item, ...map.get(item.id) });
-            map.delete(item.id);
+            map.set(item.id, { ...item, ...map.get(item.id) })
         } else {
-            result.push(item);
+            map.set(item.id, item)
         }
     });
-    map.forEach(item => result.push(item));
-    result.sort((a, b) => a.id - b.id)
-    return result;
+    return Array.from(map.values()).sort((a, b) => a.id - b.id);
+
 };
