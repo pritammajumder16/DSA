e77898582f540ad95778a7753fa40e56c9c37696
pritam16 <pritammajumder416@gmail.com>
2024-09-22 22:24:14 +0530

    Time: 366 ms (35.07%) | Memory: 101.8 MB (7.01%) - LeetSync

---
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.ts
@@ -0,0 +1,21 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type ArrayType = { "id": number } & Record<string, JSONValue>;
+
+function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
+    const minLength: number = Math.min(arr1.length, arr2.length)
+    let i: number = 0;
+    const map = new Map<number, ArrayType>();
+    arr2.forEach(item => map.set(item.id, item));
+    let result: ArrayType[] = []
+    arr1.forEach(item => {
+        if (map.has(item.id)) {
+            result.push({ ...item, ...map.get(item.id) });
+            map.delete(item.id);
+        } else {
+            result.push(item);
+        }
+    });
+    map.forEach(item => result.push(item));
+    result.sort((a,b)=>a.id-b.id)
+    return result;
+};
