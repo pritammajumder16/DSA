3c05cb1183246bc6d097cfb781acd8d1a2a8f56a
pritam16 <pritammajumder416@gmail.com>
2024-09-22 18:14:18 +0530

    Time: 66 ms (81.98%) | Memory: 50.3 MB (6.47%) - LeetSync

---
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function (functions) {
+    const myPromise = new Promise(async (res, rej) => {
+        const result = []
+        let resolvedPromisesCount = 0;
+        functions.forEach((fn, index) => {
+            fn().then(val => {
+                result[index] = (val)
+                resolvedPromisesCount++;
+                if (resolvedPromisesCount == functions.length)
+                    res(result)
+            }).catch(rej)
+        })
+    })
+    return myPromise;
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
